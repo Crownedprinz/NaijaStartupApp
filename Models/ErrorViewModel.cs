7432ce71203da6504ba16fd2e3fb01e58dbf0b9d
John Ademola <John.Ademola@sterling.ng>
2019-09-09 10:21:16 +0100

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NaijaStartupApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
