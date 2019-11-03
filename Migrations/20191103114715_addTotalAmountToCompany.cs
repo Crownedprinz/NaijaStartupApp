35617da8c1de44be076fa596dc690cba123efc57
John Ademola <John.Ademola@sterling.ng>
2019-11-03 16:33:56 +0100

    updated payment and loader for index and sign up. also ammended add on

---
+++ Migrations/20191103114715_addTotalAmountToCompany.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NaijaStartupApp.Migrations
+{
+    public partial class addTotalAmountToCompany : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalAmount",
+                table: "Company_Registration",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TotalAmount",
+                table: "Company_Registration");
+        }
+    }
+}
